d59f281fff26472d85bc4524322434451feae278
walidde94 <walidde94@users.noreply.github.com>
2026-04-05 00:29:05 +0200

    chore: remove debug test files

--- backend/test-db.ts
+++
@@ -1,8 +0,0 @@
-import { AppDataSource } from './src/config/database';
-
-AppDataSource.initialize().then(async () => {
-    const fileRepo = AppDataSource.getRepository('File');
-    const files = await fileRepo.find({ relations: ['analyses'] });
-    console.log(JSON.stringify(files.slice(0, 3), null, 2));
-    process.exit(0);
-}).catch(e => { console.error("Error:", e); process.exit(1); });
